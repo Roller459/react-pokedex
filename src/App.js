efeb80aa2b080a9ebf7803d3b4f00d40242b0541
Jose Sanchez <sanchezmoreno89@gmail.com>
2021-12-07 19:42:49 +0100

    step 1 - add search by id

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+
+import logo from "./logo.svg";
+import "./App.css";
+
+function getPokemonById(id, setPokemon) {
+  fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+    .then((response) => response.json())
+    .then((data) => setPokemon(data))
+    .catch((e) => console.log(e));
+}
 
 function App() {
+  const [pokemon, setPokemon] = useState(null);
+  const [pokemonId, setPokemonId] = useState(1);
+
+  useEffect(() => {
+    if (pokemonId !== "") getPokemonById(pokemonId, setPokemon);
+  }, [pokemonId]);
+
+  console.log({ pokemon, pokemonId });
+
+  function setPokemonIdInInput(event) {
+    setPokemonId(event.target.value);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <label>PokemonId </label>
+      <input type="number" min="1" max="898" onChange={setPokemonIdInInput} />
+      {pokemon !== null ? (
+        <>
+          <p>Name: {pokemon.name}</p>
+          <img
+            src={pokemon.sprites.front_shiny}
+            alt="pokemon"
+            width="300"
+            height="300"
+          />
+        </>
+      ) : (
+        <>Loading....</>
+      )}
     </div>
   );
 }
